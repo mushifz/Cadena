9b1396442a2b31f41902eef9702152a4e8366164
mushifz <20243rd062@utez.edu.mx>
2025-05-08 11:54:19 -0600

    Se agrego la opcion de apellidos

--- Cadena.java
+++ Cadena.java
@@ -5,7 +5,11 @@ public class Cadena {
         Scanner almacenar = new Scanner(System.in);
         System.out.println("Como se llama tu nombre ");
         String nombre = almacenar.nextLine();
-        System.out.println("Tu nombre se llama: " + nombre);
+        System.out.println("Como se llama tu apellido paterno ");
+        String apellidoPaterno = almacenar.nextLine();
+        System.out.println("Como se llama tu apellido materno ");
+        String apellidoMaterno = almacenar.nextLine();
+        System.out.println("Tu nombre es: " + nombre + " " + apellidoPaterno + " " + apellidoPaterno + " Buenos dias xd");
         
     }
 }
